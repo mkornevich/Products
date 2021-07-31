6fce345ffe0314e9ef4684b136a8b7bfe09ad95f
Maksim Kornevich <m.kornevich@itransition.com>
2021-07-31 12:58:22 +0300

    refactoring

--- src/Command/ProductsImportCommand/Process/LoadCsvProcess.php
+++ src/Command/ProductsImportCommand/Process/LoadCsvProcess.php
@@ -24,7 +24,7 @@ class LoadCsvProcess extends Process
         $csvRows = $this->loadCsvRows($filename);
 
         if (isset($csvRows[0]) && count($csvRows[0]) != ProductRow::COLUMNS_COUNT) {
-            $this->addError("In csv file, column count need to be 6.");
+            $this->addError("In csv file, column count need to be " . ProductRow::COLUMNS_COUNT . ".");
             return [];
         }
 
